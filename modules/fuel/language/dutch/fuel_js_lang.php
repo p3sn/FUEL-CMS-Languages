cc71283017b7d8901ed3cd24237adae799b81881
p3sn <github@pdriessen.nl>
2013-05-06 13:12:58 +0300

    Create fuel_js_lang.php

---
+++ modules/fuel/language/dutch/fuel_js_lang.php
@@ -0,0 +1,74 @@
+<?php 
+/*
+|--------------------------------------------------------------------------
+| Misc. For Javascript Files
+|--------------------------------------------------------------------------
+*/
+$lang['label_search'] = 'Zoeken...';
+
+$lang['btn_save'] = 'Opslaan';
+$lang['btn_cancel'] = 'Annuleren';
+
+$lang['btn_add'] = 'Toevoegen';
+$lang['btn_edit'] = 'Aanpassen';
+
+$lang['btn_select'] = 'Selecteer';
+$lang['btn_asset'] = 'Bestanden';
+$lang['btn_image'] = 'Afbeelding';
+$lang['btn_pdf'] = 'PDF';
+$lang['btn_flash'] = 'Flash';
+$lang['btn_view_source'] = 'Toon Bron';
+$lang['btn_view_editor'] = 'Toon Editor';
+
+$lang['comboselect_values_empty'] = 'Er zijn geen waarden meer om te selecteren';
+$lang['comboselect_selected_empty'] = 'Kies uit de waarden aan de linkerzijde';
+$lang['comboselect_filter'] = 'Filter je zoekopdracht';
+$lang['tree_orphans'] = '_onbestemd';
+
+$lang['confirm_delete'] = 'Wil je dit zeker verwijderen?';
+
+$lang['form_label_send_email_notification'] = 'Verstuur een wachtwoord-update per e-mail?';
+
+$lang['error_importing_ajax'] = 'Fout bij het importeren van het \'view\'-bestand';
+
+$lang['viewpage_close'] = 'Sluiten';
+
+$lang['rearrange_on'] = 'Je kan de volgorde veranderen door de rijen te slepen ten opzicht van elkaar.';
+
+/*
+|--------------------------------------------------------------------------
+| MarkItUp
+|--------------------------------------------------------------------------
+*/
+$lang['markitup_b'] = 'Vetgedrukt';
+$lang['markitup_i'] = 'Cursief';
+$lang['markitup_strike'] = 'Doorgehaald';
+$lang['markitup_p'] = 'Paragraaf';
+$lang['markitup_h1'] = 'Koptekst 1';
+$lang['markitup_h2'] = 'Koptekst 2';
+$lang['markitup_h3'] = 'Koptekst 3';
+$lang['markitup_h4'] = 'Koptekst 4';
+$lang['markitup_ul'] = 'Genummerde lijst';
+$lang['markitup_ol'] = 'Opsommingslijst';
+$lang['markitup_li'] = 'Lijst item';
+$lang['markitup_blockquote'] = 'Citaat';
+$lang['markitup_hr'] = 'Horizontale lijn';
+$lang['markitup_img'] = 'Afbeelding';
+$lang['markitup_link'] = 'Link';
+$lang['markitup_target'] = 'Doel';
+$lang['markitup_title'] = 'Titel';
+$lang['markitup_mailto'] = 'Mail naar';
+$lang['markitup_php'] = 'PHP Variable';
+$lang['markitup_clean'] = 'Opschonen';
+$lang['markitup_preview'] = 'Preview aan/uit (hold down alt/option and click to preview)';
+$lang['markitup_fullscreen'] = 'Volledig scherm';
+
+// placeholders
+$lang['markitup_placeholder_headings'] = 'Jouw titel hier...';
+$lang['markitup_placeholder_link'] = 'Je tekst hier als link…';
+$lang['markitup_email'] = 'email@domeinnaam.nl';
+$lang['markitup_fullscreen_close'] = 'Sluiten';
+
+
+/* End of file fuel_js_lang.php */
+/* Location: ./modules/fuel/language/dutch/fuel_js_lang.php */
