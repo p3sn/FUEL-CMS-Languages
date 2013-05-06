bd1d89637236cc33445c96f8328a080f49b293ff
p3sn <github@pdriessen.nl>
2013-05-06 14:43:39 +0300

    Create cronjobs_lang.php

---
+++ modules/cronjobs/language/dutch/cronjobs_lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['module_cronjobs'] = 'Cronjobs';
+
+$lang['cronjobs_instructions'] = 'Hieronder kunnen geautomatiseerde opdrachten (cronjobs) aangemaakt worden. Cronjobs zijn bijvoorbeeld ideaal om periodiek een backup te maken. We hebben zelfs al een voorbeeld voor je klaar staan op <strong>'.INSTALL_ROOT.'crons'.DIRECTORY_SEPARATOR.'crontab_default.php</strong>.
+Voor meer informatie <a href="http://www.google.com/search?client=safari&rls=en-us&q=cron+job+tutorial&ie=UTF-8&oe=UTF-8" target="_blank"><strong>over cronjobs kun je hier klikken.</strong></a>.';
+
+$lang['btn_remove_cronjobs'] = 'Cron Job(s) Verwijderen';
+$lang['btn_save_cronjobs'] = 'Cron Job(s) Opslaan';
+
+$lang['cronjobs_mailto'] = 'Mail aan:';
+$lang['cronjobs_min'] = 'min';
+$lang['cronjobs_hour'] = 'uur';
+$lang['cronjobs_month_day'] = 'maand dag';
+$lang['cronjobs_month_num'] = 'maand num';
+$lang['cronjobs_week_day'] = 'week dag';
+$lang['cronjobs_command'] = 'opdracht';
+
+/* End of file cronjobs_lang.php */
+/* Location: ./modules/cronjobs/language/dutch/cronjobs_lang.php */
