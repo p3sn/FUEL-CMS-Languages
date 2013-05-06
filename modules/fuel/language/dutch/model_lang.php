7d930f9ba74cb160dcb410c44d89c374de9ac662
p3sn <github@pdriessen.nl>
2013-05-06 13:14:11 +0300

    Create model_lang.php

---
+++ modules/fuel/language/dutch/model_lang.php
@@ -0,0 +1,21 @@
+<?php 
+// model specific error messages
+$lang['error_val_empty_or_already_exists'] = 'De %1s is leeg of bestaat al.';
+$lang['error_location_parents_match'] = 'Locatie en parents kunnen niet gelijk zijn.';
+$lang['error_in_readonly_mode'] = 'De %1s is alleen-lezen, de data kan niet gewijzigd worden.';
+
+// standard model errors
+$lang['error_valid_email'] = 'Voer een geldig e-mail adres in.';
+$lang['error_valid_phone'] = 'Voer een geldig telefoonnummer in.';
+$lang['error_method_does_not_exist'] = "Methode %1s bestaat niet.";
+
+$lang['error_value_exceeds_length'] = 'Waarde is groter dan de benodigde lengte voor %1s';
+$lang['error_not_number'] = 'Waarde moet een nummer zijn voor %1s';
+$lang['error_invalid_date'] = 'Ongeldige datum voor %1s';
+$lang['error_invalid_time'] = 'Ongeldige tijd voor %1s';
+$lang['error_invalid_year'] = 'Ongeldig jaar voor %1s';
+$lang['error_invalid_generic'] = 'Ongeldige waarde voor %1s';
+
+
+/* End of file model_lang.php */
+/* Location: ./modules/fuel/language/english/model_lang.php */
