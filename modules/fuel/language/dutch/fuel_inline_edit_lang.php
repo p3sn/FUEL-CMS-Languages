846899edb4b7a3553db56bae9a79c3cca4a0e348
p3sn <github@pdriessen.nl>
2013-05-06 13:12:27 +0300

    Create fuel_inline_edit_lang.php for Dutch

---
+++ modules/fuel/language/dutch/fuel_inline_edit_lang.php
@@ -0,0 +1,21 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Inline Edit Toolbar
+|--------------------------------------------------------------------------
+*/
+$lang['inline_edit_toggle_toolbar'] = 'Toolbar aan/uit';
+$lang['inline_edit_toggle_editable'] = 'Bewerkbare gebieden aan/uit';
+$lang['inline_edit_toggle_publish'] = 'Publiceer status aan/uit';
+$lang['inline_edit_toggle_cache'] = 'Pagina cache aan/uit';
+
+$lang['inline_edit_other_pages'] = 'Andere pagina\'s…';
+$lang['inline_edit_layout'] = 'Layout';
+
+$lang['inline_edit_back_to_admin'] = 'Terug naar Admin';
+$lang['inline_edit_logout'] = 'Afmelden';
+$lang['inline_edit_logout_title'] = 'Afmelden van Fuel Admin';
+$lang['inline_edit_page_not_published'] = 'Deze pagina is niet gepubliceerd';
+
+/* End of file fuel_inline_edit_lang.php */
+/* Location: ./modules/fuel/language/english/fuel_inline_edit_lang.php */
